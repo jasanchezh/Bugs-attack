76addc5ddb20792d2c5e72db3b2fc551ca4253f2
Jose Sanchez <joseansanher@gmail.com>
2018-01-22 17:52:05 +0100

    divs added

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+/*window.onload = function() {
   document.getElementById("start-button").onclick = function() {
     startGame();
   };
@@ -6,6 +6,18 @@ window.onload = function() {
 function startGame(){
 var canvas = document.getElementById('example');
 var ctx = canvas.getContext('2d');
+var background = new Image();
+  background.src = 'images/background.png';
+  ctx.drawImage(bg, 0, 0,1000,800);
+  var bug = new Image();
+  bug.src = 'images/bug.png';
+  ctx.drawImage(bg, 0, 0,1000,800);
+    }
 
-  }
-};
+function draw() {
+  ctx.beginPath();
+  ctx.bug(50, 50, 10, 0, Math.PI*2);
+  ctx.closePath();
+}
+setInterval(draw, 10);
+};*/
